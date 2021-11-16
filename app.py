12da67e6602b64a0fd9a9737c5f5d156b8abc2d0
Eltody <tomas.zatko.ms@gmail.com>
2021-11-16 03:26:59 +0100

    added BE for connections search

--- app.py
+++ app.py
@@ -22,9 +22,72 @@ def index():
 def busConfig():
     fromCity = request.form['fromCity']
     toCity = request.form['toCity']
-    #TODO connect to database, return list of connections
-    #TODO list connections
-    return render_template('busList.html')
+    timeFromDate = request.form['date']
+
+    # ziskanie id fromCity a toCity
+    cursor1 = connection.cursor()
+    cursor2 = connection.cursor()
+    cursor1.execute("SELECT id FROM Zastavky WHERE nazov_zastavky='%s';" % fromCity)
+    cursor2.execute("SELECT id FROM Zastavky WHERE nazov_zastavky='%s';" % toCity)
+    id_fromCity = cursor1.fetchone()
+    id_toCity = cursor2.fetchone()
+    cursor1.close()
+    cursor2.close()
+    id_fromCity = str(id_fromCity[0])    # id mesta, z ktoreho cestujem
+    id_toCity = str(id_toCity[0])        # id mesta, do ktoreho cestujem
+
+    # ziskanie casov prejazdov z jednotlivych miest podla id fromCity a toCity
+    cursor1 = connection.cursor()
+    cursor2 = connection.cursor()
+    cursor1.execute("SELECT cas_prejazdu, id_spoju FROM Spoj_Zastavka WHERE id_zastavky='%s';" % id_fromCity)
+    cursor2.execute("SELECT cas_prejazdu, id_spoju FROM Spoj_Zastavka WHERE id_zastavky='%s';" % id_toCity)
+    possibilities_fromCity = cursor1.fetchall()
+    possibilities_toCity = cursor2.fetchall()
+    cursor1.close()
+    cursor2.close()
+
+    # ziskanie konkretneho casu pre vyhladanie najblizsich spojeni
+    timeFromDate = timeFromDate.rsplit('T', 1)         # splitnutie na datum [0] a cas [1]
+    timeFromDate = timeFromDate[1].replace(":", "")    # odstranenie ':' pre prevod na int
+    timeFromDate = int(timeFromDate)    # string to int pre porovanie casov
+
+    #### VYSVETLENIE PREMENNYCH ####
+    # fromCity - mesto, odkial cestujem
+    # toCity - mesto, kam cestujem
+    # possibilities_fromCity - casy prejazdov z mesta, odkial cestujem, s id_spojom
+    # possibilities_toCity - casy prejazdov z mesta, kam cestujem, s id_spojom
+    # timeFromDate - konkretny cas zadany uzivatelom vo vyhladavani spojov
+
+    # ziskanie len vyhovujucich spojov - rovnake id_spoju pre fromCity a toCity
+    possibleBusConnections = []
+    for row1 in possibilities_fromCity:
+        for row2 in possibilities_toCity:
+            if row1[1] == row2[1]:  # rovnanie cisla spojov v jednotlivych casoch prejazdov
+                tmp_timeFromCity = str(row1[0]) # array to string
+                tmp_timeToCity = str(row2[0])
+                tmp_timeFromCity = tmp_timeFromCity.replace(":", "")    # odstranenie ':' pre porovnanie casov
+                tmp_timeToCity = tmp_timeToCity.replace(":", "")
+                tmp_timeFromCity = int(tmp_timeFromCity)    # string to int
+                tmp_timeToCity = int(tmp_timeToCity)
+                if tmp_timeFromCity < tmp_timeToCity:
+                    cursor1 = connection.cursor()
+                    cursor1.execute("SELECT id_dopravca_spoje FROM Spoj WHERE id='%s';" % row1[1])
+                    id_dopravca = cursor1.fetchone()    # ziskanie id_dopravcu_spoje
+                    cursor1.close()
+                    cursor1 = connection.cursor()
+                    cursor1.execute("SELECT meno, priezvisko FROM Dopravca WHERE id='%s';" % id_dopravca)
+                    carrier_name = cursor1.fetchone()  # ziskanie id_dopravcu_spoje
+                    cursor1.close()
+                    fromCityTime = row1[0]
+                    toCityTime = row2[0]
+                    connectionNumber = row1[1]
+                    carrier_name = carrier_name[0] + carrier_name[1]  # spojenie meno a priezvisko do jedneho
+                    if tmp_timeFromCity > timeFromDate: # porovnanie casu odchodu a zvoleneho casu uzivatelom pre najblizsie spoje
+                        possibleBusConnections.append([connectionNumber, fromCity, fromCityTime, toCity, toCityTime, carrier_name])
+    print(possibleBusConnections)
+    # possibleBusConnections - vo formate: cislo_spoju, fromCity, cas_prejazdu(fromCity), toCity, cas_prejazdu(toCity), dopravca
+
+    return render_template('connections.html')
 
 
 @app.route('/preSignIn', methods=['GET', 'POST'])
