e854e2b0e45184b6994097e8ed686d1dbb30a3b2
Eltody <tomas.zatko.ms@gmail.com>
2021-11-18 01:09:59 +0100

    BE - added functionality for bus connections - 5 connections per click, searching for next connections

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 connection = pymysql.connect(host='92.52.58.251', user='admin', password='password', db='iis')
 emails = []
 loginData = {}
+
+
 #############################################
 @app.route('/')
 def index():
@@ -21,11 +23,19 @@ def index():
     loginData.clear()
     return render_template("index.html", cities=cities, data=data)
 
-@app.route('/busConfig', methods=['GET', 'POST'])
-def busConfig():
-    fromCity = request.form['fromCity']
-    toCity = request.form['toCity']
-    timeFromDate = request.form['date']
+
+@app.route('/busConfig/<boolLoadMore>/<lastConnectionOnWeb>', methods=['GET', 'POST'])
+def busConfig(boolLoadMore, lastConnectionOnWeb):
+    if boolLoadMore == 'False':
+        fromCity = request.form['fromCity']
+        toCity = request.form['toCity']
+        timeFromDate = request.form['date']
+    elif boolLoadMore == True:    # z funkcie LoadMore: nacitanie dalsich spojov, lastConnectionOnWeb je posledny zobrazeny prvok na stranke
+        lastConnectionOnWeb = lastConnectionOnWeb[1:-1]
+        lastConnectionOnWeb = lastConnectionOnWeb.split(',')
+        fromCity = lastConnectionOnWeb[1]
+        toCity = lastConnectionOnWeb[3]
+        timeFromDate = lastConnectionOnWeb[2]
 
     # ziskanie id fromCity a toCity
     cursor1 = connection.cursor()
@@ -36,8 +46,8 @@ def busConfig():
     id_toCity = cursor2.fetchone()
     cursor1.close()
     cursor2.close()
-    id_fromCity = str(id_fromCity[0])    # id mesta, z ktoreho cestujem
-    id_toCity = str(id_toCity[0])        # id mesta, do ktoreho cestujem
+    id_fromCity = str(id_fromCity[0])  # id mesta, z ktoreho cestujem
+    id_toCity = str(id_toCity[0])  # id mesta, do ktoreho cestujem
 
     # ziskanie casov prejazdov z jednotlivych miest podla id fromCity a toCity
     cursor1 = connection.cursor()
@@ -50,10 +60,15 @@ def busConfig():
     cursor2.close()
 
     # ziskanie konkretneho casu pre vyhladanie najblizsich spojeni
-    timeFromDate = timeFromDate.rsplit('T', 1)         # splitnutie na datum [0] a cas [1]
-    tmp_dateOfConnection = timeFromDate[0]  # datum daneho spoju
-    timeFromDate = timeFromDate[1].replace(":", "")    # odstranenie ':' pre prevod na int
-    timeFromDate = int(timeFromDate)    # string to int pre porovanie casov
+    if boolLoadMore == 'False':
+        timeFromDate = timeFromDate.rsplit('T', 1)  # splitnutie na datum [0] a cas [1]
+        tmp_dateOfConnection = timeFromDate[0]  # datum daneho spoju
+        timeFromDate = timeFromDate[1].replace(":", "")  # odstranenie ':' pre prevod na int
+    else:
+        timeFromDate = timeFromDate.replace(":", "")  # odstranenie ':' pre prevod na int
+        splittedDate = lastConnectionOnWeb[7].split('.')    # den [0], mesiac [1], konkretny den v tyzdni [2]
+        tmp_dateOfConnection = '2021-' + splittedDate[1] + '-' + splittedDate[0]
+    timeFromDate = int(timeFromDate)  # string to int pre porovanie casov
 
     #### VYSVETLENIE PREMENNYCH ####
     # fromCity - mesto, odkial cestujem
@@ -65,26 +80,31 @@ def busConfig():
     # ziskanie len vyhovujucich spojov - rovnake id_spoju pre fromCity a toCity
     possibleBusConnections = []
     laterPossibleBusConnections = []
-    nextDay = False # priznak pre opatovne vykovanie celeho cyklu, ale pre dalsi den spojeni
-    counterOfConnections = 0 # counter pre pocet spojov zobrazenych sa jednu stranku
+    nextDay = False  # priznak pre opatovne vykovanie celeho cyklu, ale pre dalsi den spojeni
+    counterOfConnections = 0  # counter pre pocet spojov zobrazenych sa jednu stranku
     for x in range(2):
-        if x == 1:      # druhe vykonanie for cyklu - druhe hladanie v databazi - rovnake, len datum sa zvysi o 1
+        if x == 1:  # druhe vykonanie for cyklu - druhe hladanie v databazi - rovnake, len datum sa zvysi o 1
             nextDay = True
         for row1 in possibilities_fromCity:
+            if counterOfConnections >= 5:
+                continue
             for row2 in possibilities_toCity:
+                if counterOfConnections >= 5:
+                    continue
                 if row1[1] == row2[1]:  # rovnanie cisla spojov v jednotlivych casoch prejazdov pre vyber z DB
-                    tmp_timeFromCity = str(row1[0]) # array to string
+                    tmp_timeFromCity = str(row1[0])  # array to string
                     tmp_timeToCity = str(row2[0])
-                    tmp_secsTimeFromCity = str(row1[0]) # premenna pre prevod casu na sekundy kvoli vypoctu trvania cesty
-                    tmp_secsTimeToCity = str(row2[0])   # premenna pre prevod casu na sekundy kvoli vypoctu trvania cesty
-                    tmp_timeFromCity = tmp_timeFromCity.replace(":", "")    # odstranenie ':' pre porovnanie casov
+                    tmp_secsTimeFromCity = str(
+                        row1[0])  # premenna pre prevod casu na sekundy kvoli vypoctu trvania cesty
+                    tmp_secsTimeToCity = str(row2[0])  # premenna pre prevod casu na sekundy kvoli vypoctu trvania cesty
+                    tmp_timeFromCity = tmp_timeFromCity.replace(":", "")  # odstranenie ':' pre porovnanie casov
                     tmp_timeToCity = tmp_timeToCity.replace(":", "")
-                    tmp_timeFromCity = int(tmp_timeFromCity)    # string to int
+                    tmp_timeFromCity = int(tmp_timeFromCity)  # string to int
                     tmp_timeToCity = int(tmp_timeToCity)
                     if tmp_timeFromCity < tmp_timeToCity:
                         cursor1 = connection.cursor()
                         cursor1.execute("SELECT id_dopravca_spoje FROM Spoj WHERE id='%s';" % row1[1])
-                        id_dopravca = cursor1.fetchone()    # ziskanie id_dopravcu_spoje
+                        id_dopravca = cursor1.fetchone()  # ziskanie id_dopravcu_spoje
                         cursor1.close()
 
                         cursor1 = connection.cursor()
@@ -105,10 +125,11 @@ def busConfig():
                         fromCityTime = row1[0]
                         toCityTime = row2[0]
                         connectionNumber = row1[1]
-                        availableSeats = availableSeats[0] # array to string
+                        availableSeats = availableSeats[0]  # array to string
                         carrier_name = carrier_name[0] + carrier_name[1]  # spojenie meno a priezvisko do jedneho
 
-                        tmp_secsTimeFromCity = tmp_secsTimeFromCity.rsplit(':', 1)  # splitnutie na hodiny [0] a minuty [1]
+                        tmp_secsTimeFromCity = tmp_secsTimeFromCity.rsplit(':',
+                                                                           1)  # splitnutie na hodiny [0] a minuty [1]
                         tmp_secsTimeToCity = tmp_secsTimeToCity.rsplit(':', 1)  # splitnutie na hodiny [0] a minuty [1]
 
                         tmp_HoursSecsFromCity = int(tmp_secsTimeFromCity[0]) * 3600  # hodiny v sekudach
@@ -116,8 +137,10 @@ def busConfig():
 
                         tmp_HoursSecsToCity = int(tmp_secsTimeToCity[0]) * 3600  # hodiny v sekudach
                         tmp_MinutesSecsToCity = int(tmp_secsTimeToCity[1]) * 60  # minuty v sekudach
-                        connectionTimeHours = ((tmp_HoursSecsToCity + tmp_MinutesSecsToCity) - (tmp_HoursSecsFromCity + tmp_MinutesSecsFromCity)) / 3600 # dlzka trvania spoju v hodinach
-                        connectionTimeMinutes = ((tmp_HoursSecsToCity + tmp_MinutesSecsToCity) - (tmp_HoursSecsFromCity + tmp_MinutesSecsFromCity)) / 60  # dlzka trvania spoju v minutach
+                        connectionTimeHours = ((tmp_HoursSecsToCity + tmp_MinutesSecsToCity) - (
+                                    tmp_HoursSecsFromCity + tmp_MinutesSecsFromCity)) / 3600  # dlzka trvania spoju v hodinach
+                        connectionTimeMinutes = ((tmp_HoursSecsToCity + tmp_MinutesSecsToCity) - (
+                                    tmp_HoursSecsFromCity + tmp_MinutesSecsFromCity)) / 60  # dlzka trvania spoju v minutach
                         connectionTimeHours = float(connectionTimeHours)
 
                         # prevod hodin na hodiny a minuty
@@ -136,25 +159,41 @@ def busConfig():
 
                         # ziskanie konkretneho dna z datumu
                         tmp2_dateOfConnection = str(tmp_dateOfConnection)
-                        tmp2_dateOfConnection = tmp2_dateOfConnection.rsplit('-', 1)  # splitnutie na rok a mesiac [0] a den [1]
+                        tmp2_dateOfConnection = tmp2_dateOfConnection.rsplit('-',
+                                                                             1)  # splitnutie na rok a mesiac [0] a den [1]
                         tmp_unsplittedYearAndMonth = str(tmp2_dateOfConnection[0])
                         tmp_yearAndMonth = tmp_unsplittedYearAndMonth.rsplit('-', 1)  # splitnutie na rok [0] mesiac [1]
-                        ans = datetime.date(int(tmp_yearAndMonth[0]), int(tmp_yearAndMonth[1]), int(tmp2_dateOfConnection[1]))
+                        ans = datetime.date(int(tmp_yearAndMonth[0]), int(tmp_yearAndMonth[1]),
+                                            int(tmp2_dateOfConnection[1]))
                         dayOfConnection = ans.strftime("%A")
                         if dayOfConnection == 'Monday':
                             dayOfConnection = 'po'
+                            if nextDay:
+                                dayOfConnection = 'ut'
                         elif dayOfConnection == 'Tuesday':
                             dayOfConnection = 'ut'
+                            if nextDay:
+                                dayOfConnection = 'str'
                         elif dayOfConnection == 'Wednesday':
                             dayOfConnection = 'st'
+                            if nextDay:
+                                dayOfConnection = ''.join(symbols[0]) + 't'
                         elif dayOfConnection == 'Thursday':
                             dayOfConnection = ''.join(symbols[0]) + 't'
+                            if nextDay:
+                                dayOfConnection = 'pi'
                         elif dayOfConnection == 'Friday':
                             dayOfConnection = 'pi'
+                            if nextDay:
+                                dayOfConnection = 'so'
                         elif dayOfConnection == 'Saturday':
                             dayOfConnection = 'so'
+                            if nextDay:
+                                dayOfConnection = 'ne'
                         elif dayOfConnection == 'Sunday':
                             dayOfConnection = 'ne'
+                            if nextDay:
+                                dayOfConnection = 'po'
 
                         # ak uz prebieha hlavny for po 2.krat (druhy priebeh databazy - pre ostatne dni, nie len najblizsie spoje) tak sa zvysi datum o 1 vyssie
                         if nextDay:
@@ -162,7 +201,8 @@ def busConfig():
                             tmp2_dateOfConnection[1] += x
                             tmp2_dateOfConnection[1] = str(tmp2_dateOfConnection[1])
                         # dokopy den, mesiac a konkretny den
-                        dateAndDayOfConnection = tmp2_dateOfConnection[1] + '.' + tmp_yearAndMonth[1] + '. ' + dayOfConnection
+                        dateAndDayOfConnection = tmp2_dateOfConnection[1] + '.' + tmp_yearAndMonth[
+                            1] + '. ' + dayOfConnection
 
                         # vypocitanie ceny na zaklade doby trvania spoju v minutach a ceny za jednu minutu
                         int(connectionTimeMinutes)
@@ -176,27 +216,35 @@ def busConfig():
                         if tmp_timeToCity < 959:
                             toCityTime = ' ' + toCityTime
 
-                        if tmp_timeFromCity > timeFromDate and not nextDay: # porovnanie casu odchodu a zvoleneho casu uzivatelom pre najblizsie spoje
-                            possibleBusConnections.append([connectionNumber, fromCity, fromCityTime, toCity, toCityTime, carrier_name, availableSeats, dateAndDayOfConnection, connectionTimeHours, priceOfConnection])
+                        if tmp_timeFromCity > timeFromDate and not nextDay and counterOfConnections < 5:  # porovnanie casu odchodu a zvoleneho casu uzivatelom pre najblizsie spoje
+                            possibleBusConnections.append(
+                                [connectionNumber, fromCity, fromCityTime, toCity, toCityTime, carrier_name,
+                                 availableSeats, dateAndDayOfConnection, connectionTimeHours, priceOfConnection])
                             counterOfConnections += 1
-                        if nextDay:    # pre dalsie spoje, na dalsi den - rovnake spoje, len datum o cislo vyssi a od zaciatku dna 00:00 vsetky, nie len najblizsie v dany den
-                            laterPossibleBusConnections.append([connectionNumber, fromCity, fromCityTime, toCity, toCityTime, carrier_name, availableSeats, dateAndDayOfConnection, connectionTimeHours, priceOfConnection])
+                        if nextDay and counterOfConnections < 5:  # pre dalsie spoje, na dalsi den - rovnake spoje, len datum o cislo vyssi a od zaciatku dna 00:00 vsetky, nie len najblizsie v dany den
+                            laterPossibleBusConnections.append(
+                                [connectionNumber, fromCity, fromCityTime, toCity, toCityTime, carrier_name,
+                                 availableSeats, dateAndDayOfConnection, connectionTimeHours, priceOfConnection])
                             counterOfConnections += 1
 
-    possibleBusConnections = possibleBusConnections + laterPossibleBusConnections # spojenie najblizsich vyhovujucich spojov a spojov pre dalsi den napr
+    possibleBusConnections = possibleBusConnections + laterPossibleBusConnections  # spojenie najblizsich vyhovujucich spojov a spojov pre dalsi den napr
     print(possibleBusConnections)
-                        # possibleBusConnections - vo formate: cislo_spoju, fromCity, cas_prejazdu(fromCity), toCity, cas_prejazdu(toCity), dopravca, pocet volnych miest, datum spoju, doba trvania spoju
+    # possibleBusConnections - vo formate: cislo_spoju, fromCity, cas_prejazdu(fromCity), toCity, cas_prejazdu(toCity), dopravca, pocet volnych miest, datum spoju, doba trvania spoju
 
     return render_template('connections.html', data=possibleBusConnections)
 
+
 @app.route('/loadMore/<x>')
 def loadMore(x):
-    print(x)
+    boolLoadMore = True
+    busConfig(boolLoadMore, x)
     return "done"
+
 
 @app.route('/preSignIn', methods=['GET', 'POST'])
 def preSignIn():
     return render_template('signIn.html', data="")
+
 
 @app.route('/signIn', methods=['GET', 'POST'])
 def signIn():
@@ -229,25 +277,25 @@ def signIn():
     administrator.close()
     personal.close()
 
-#    if user_email == emails[0]:
-#        if user_email == emails[1]:
-#            if user_email == emails[2]:
-#                #TODO email sending
-#            elif emails[2] == '':
-#                emails.append(user_email)
-#            else:
-#                emails.clear()
-#                emails.append(user_email)
-#        elif emails[1] == '':
-#            emails.append(user_email)
-#        else:
-#            emails.clear()
-#            emails.append(user_email)
-#    elif emails[0] == '':
-#        emails.append(user_email)
-#    else:
-#        emails.clear()
-#        emails.append(user_email)
+    #    if user_email == emails[0]:
+    #        if user_email == emails[1]:
+    #            if user_email == emails[2]:
+    #                #TODO email sending
+    #            elif emails[2] == '':
+    #                emails.append(user_email)
+    #            else:
+    #                emails.clear()
+    #                emails.append(user_email)
+    #        elif emails[1] == '':
+    #            emails.append(user_email)
+    #        else:
+    #            emails.clear()
+    #            emails.append(user_email)
+    #    elif emails[0] == '':
+    #        emails.append(user_email)
+    #    else:
+    #        emails.clear()
+    #        emails.append(user_email)
     data = {'error': 'log', 'email': user_email}
     return render_template('signIn.html', data=data)
 
@@ -271,12 +319,14 @@ def registration():
     cursor.close()
 
     cursor = connection.cursor()
-    cursor.execute("insert into `Cestujuci` (meno, priezvisko, email, heslo) VALUES (%s, %s, %s, %s)", (fname, lname, user_email, password))
+    cursor.execute("insert into `Cestujuci` (meno, priezvisko, email, heslo) VALUES (%s, %s, %s, %s)",
+                   (fname, lname, user_email, password))
     connection.commit()
     cursor.close()
 
     loginData = {'message': 'login', 'email': user_email, 'name': fname}
     return index()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
