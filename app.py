d5ea02672705e2fbb1ec07de58b6ec9e422f278b
Eltody <tomas.zatko.ms@gmail.com>
2021-11-27 18:32:01 +0100

    deleted unused sending of stop suggestions

--- app.py
+++ app.py
@@ -1402,33 +1402,9 @@ def administratorEditor():
 
     availablePersonalAndVehicles = [availableVehicles, availablePersonal]
 
-    # ziskanie vsetkych id nepotvrdenych navrhov zastavok od dopravcov
-    cursor = connection.cursor()
-    cursor.execute("SELECT id from NavrhZastavky WHERE stav='nepotvrdena'")
-    tmp_allIdsSuggestions = cursor.fetchall()
-    cursor.close()
-    allIdsSuggestions = []
-    for m in tmp_allIdsSuggestions:
-        for n in m:
-            allIdsSuggestions.append(n)
-
-    # ziskanie vsetkych nepotvrdenych navrhov zastavok od dopravcov
-    allSuggestions = []
-    for i in allIdsSuggestions:
-        cursor = connection.cursor()
-        cursor.execute(
-            "SELECT nazov, geograficka_poloha, id_dopravca_navrhy from NavrhZastavky WHERE stav='nepotvrdena' and id='%s';" % i)
-        tmp_oneSuggestion = cursor.fetchall()
-        cursor.close()
-        oneSuggestion = []
-        for m in tmp_oneSuggestion:
-            for n in m:
-                oneSuggestion.append(n)
-        allSuggestions.append(oneSuggestion)
-
     data = {'vehicles': allVehicles, 'connections': allConnections,
             'personal': allPersonal,
-            'availablePersonalAndVehicles': availablePersonalAndVehicles, 'suggestions': allSuggestions,
+            'availablePersonalAndVehicles': availablePersonalAndVehicles,
             'carrierName': carrierName}
     print(data)
     return render_template("administratorEditor.html", data=data, cities=allNamesOfCities)
